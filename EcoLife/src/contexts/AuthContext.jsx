aea288f5cc30d961483818b4a064da432c188c34
Luan <luanlq.24ite@vku.udn.vn>
2025-08-27 19:37:28 +0700

    fix Auth

--- EcoLife/src/contexts/AuthContext.jsx
+++ EcoLife/src/contexts/AuthContext.jsx
@@ -1,5 +1,5 @@
-import React, { createContext, useContext } from 'react';
-import { useAuth } from '../hooks/useAuth';
+import { createContext } from "react";
+import { useAuth } from "../hooks/useAuth";
 
 // Create the context
 const AuthContext = createContext(null);
@@ -7,19 +7,6 @@ const AuthContext = createContext(null);
 // Create a provider component
 export const AuthProvider = ({ children }) => {
   const auth = useAuth();
-  
-  return (
-    <AuthContext.Provider value={auth}>
-      {children}
-    </AuthContext.Provider>
-  );
-};
 
-// Create a hook to use the auth context
-export const useAuthContext = () => {
-  const context = useContext(AuthContext);
-  if (context === null) {
-    throw new Error('useAuthContext must be used within an AuthProvider');
-  }
-  return context;
+  return <AuthContext.Provider value={auth}>{children}</AuthContext.Provider>;
 };
